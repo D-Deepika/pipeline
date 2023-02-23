6bd877c6a6ff774a2b043a82391e227f6ca67502
deepika200326 <20A91A0476@aec.edu.in>
2023-02-23 15:06:13 +0530

    hi

--- pythonflask/app.py
+++ pythonflask/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
   
 @app.route('/')
 def hello():
-    return "welcome to the flask tutorials by deepika"
+    return "welcome to the flask tutorials"
   
   
 if __name__ == "__main__":
